de7e07859f815e0b5101c646cd1f9d1c438df2ea
Joseph Scherreik <joe.scherreik@gmail.com>
2026-03-12 21:19:11 -0400

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { reactRouterDevTools } from "react-router-devtools";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -7,5 +8,10 @@ export default defineConfig({
   ssr: {
     noExternal: ["@mui/*"],
   },
-  plugins: [reactRouterDevTools(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouterDevTools(),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 });
